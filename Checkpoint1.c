315eb74585369a31bab7573a68c67aba0f599dde
basuru123 <31835925+basuru123@users.noreply.github.com>
2017-09-15 15:07:46 +1000

    Update Checklist1.c
    Added more functions and described their functionalities to meet the main requirements of the assignment.

--- Checkpoint1.c
+++ Checkpoint1.c
@@ -1,8 +1,8 @@
 /*******************************************************************************
-48430 Embedded C - Assignment 1
+48430 Embedded C - Assignment 3
 Names: Sanjeet Sharma, Sathira Wickramanayaka, Basuru Sooriyaarachchi, 
 Jai Ghorpade
-Student ID: 11742501, , ,12916110
+Student ID: 11742501,11825193,11841398,12916110 
 Date of submission: 15/09/2017
 *******************************************************************************/
 
@@ -38,10 +38,13 @@ typedef struct accountdata accountdata_t;
 Function prototypes
 *******************************************************************************/
 void printMenu();
-void inputPassData(accountdata_t* adp, char account[], char username[],
-                   char password[]);
+void selectMenu();
+void inputPassData();
 void encryptData();
-void decryptData();
+void displayData();
+void decryptFile();
+void compressFile();
+
 /*******************************************************************************
 Main
 *******************************************************************************/
@@ -49,17 +52,85 @@ int main(void)
 {
 
 }
+
+/*******************************************************************************
+Print Menu
+The purpose of this function is to print the menu options and display the 
+information required from the user.
+*******************************************************************************/
+void printMenu()
+{
+
+}
+
+/*******************************************************************************
+Select Menu
+The purpose of this function is select between the 2 modes of operation.Saving 
+passwords and uploading confidential documents.
+*******************************************************************************/
+void selectMenu()
+{
+
+}
+
+/*******************************************************************************
+Input Pass Data
+This function is called in the first mode of operation of this program.Its 
+function is to get the password details from the user and save it in a file.
+*******************************************************************************/
+void inputPassData()
+{
+
+}
+
 /*******************************************************************************
 Data Encryption
+The purpose of this function is to encrypt the file containing password 
+details.
 *******************************************************************************/
-int encryptData()
+void encryptData()
 {
                
 }
 /*******************************************************************************
 Data De-encryption
+The purpose of this function is to decrypt the encrypted file containing password
+details.
 *******************************************************************************/
-int decryptData()
+void decryptData()
 {
                
 }
+
+/*******************************************************************************
+Display Data
+This function is used in the firstmode of operation of the program.It called 
+after decrypt data to display the required data.
+*******************************************************************************/
+void displayData()
+{
+               
+}
+
+/*******************************************************************************
+Compress Data 
+This function is called in the second mode of operation of this program.Its 
+function is to compress confidential pdfs and photos uploaded by the user.
+*******************************************************************************/
+void compressFile()
+{
+
+}
+
+/*******************************************************************************
+Decompress Data 
+This function is called when the user wants to access confidential pdfs and 
+photos.The file is first decrypted and this function is called afterwards to
+decompress the files.
+*******************************************************************************/
+void decompressFile()
+
+
+
+
+
